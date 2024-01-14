165801fad6a1a31000691639611eb149bf7d4b8c
yura-prots <yuraprots@gmail.com>
2024-01-14 19:42:30 +0200

    task-1 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,15 +7,27 @@ class App extends Component {
     bad: 0,
   };
 
-  render() {
-    console.log(this.state);
+  addFeedback = e => {
+    console.log(e.target.name);
 
+    this.setState(prevState => {
+      return { [e.target.name]: prevState[e.target.name] + 1 };
+    });
+  };
+
+  render() {
     return (
       <div>
         <h1>Please leave feedback</h1>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+        <button type="button" name="good" onClick={this.addFeedback}>
+          Good
+        </button>
+        <button type="button" name="neutral" onClick={this.addFeedback}>
+          Neutral
+        </button>
+        <button type="button" name="bad" onClick={this.addFeedback}>
+          Bad
+        </button>
         <h2>Statistics</h2>
         <p>Good: {this.state.good}</p>
         <p>Neutral: {this.state.neutral}</p>
