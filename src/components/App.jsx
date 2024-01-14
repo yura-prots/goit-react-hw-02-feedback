97d72d3da6b6cc0430a24549076dcc9da1a9d327
yura-prots <yuraprots@gmail.com>
2024-01-14 15:54:16 +0200

    add basic markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    console.log(this.state);
+
+    return (
+      <div>
+        <h1>Please leave feedback</h1>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+        <h2>Statistics</h2>
+        <p>Good: {this.state.good}</p>
+        <p>Neutral: {this.state.neutral}</p>
+        <p>Bad: {this.state.bad}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
