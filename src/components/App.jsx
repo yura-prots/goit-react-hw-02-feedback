22927b1d6906ee5dbac3911aa05ce73930f7499d
yura-prots <yuraprots@gmail.com>
2024-01-15 19:31:52 +0200

    task-3 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react';
 
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
 
 class App extends Component {
   state = {
@@ -33,19 +34,22 @@ class App extends Component {
 
     return (
       <div>
-        <h1>Please leave feedback</h1>
-        <FeedbackOptions
-          options={{ good, neutral, bad }}
-          onLeaveFeedback={this.addFeedback}
-        ></FeedbackOptions>
+        <Section title={'Please leave feedback'}>
+          <FeedbackOptions
+            options={{ good, neutral, bad }}
+            onLeaveFeedback={this.addFeedback}
+          ></FeedbackOptions>
+        </Section>
 
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()}
-        ></Statistics>
+        <Section title={'Statistics'}>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          ></Statistics>
+        </Section>
       </div>
     );
   }
